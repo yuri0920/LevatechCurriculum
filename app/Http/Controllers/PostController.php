81b64f305d39e3bcb8d7fc98876d3b3089fe133f
yuri0920 <riuyuramukana0920@gmail.com>
2024-07-23 22:08:10 +0900

    curriculum08-3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,12 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+    return view('posts.show')->with(['post' => $post]);
     }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index']);
 
+Route::get('/posts/{post}', [PostController::class, 'show']);
+
