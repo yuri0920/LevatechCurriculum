eb26b9f2bba51af2e9ac95f5d5cbed09df301934
yuri0920 <riuyuramukana0920@gmail.com>
2024-07-25 17:26:39 +0900

    curriculum08-5

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -14,11 +14,11 @@
                 @method('PUT')
                 <div class='content_title'>
                     <h2>タイトル</h2>
-                    <input type='text' namme='post[title]' value='{{ $post->title }}'>
+                    <input type='text' name='post[title]' value='{{ $post->title }}'>
                 </div>
                  <div class='content_body'>
                     <h2>本文</h2>
-                    <input type='text' namme='post[body]' value='{{ $post->body }}'>
+                    <input type='text' name='post[body]' value='{{ $post->body }}'>
                 </div>
                 <input type='submit' value='保存'>
             </form>
