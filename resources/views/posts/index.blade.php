196e3fb04d76b51ff775ea2c25d81f9dd31dadf3
yuri0920 <riuyuramukana0920@gmail.com>
2024-07-25 22:48:26 +0900

    curriculum08-6

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,9 +11,13 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
-                        <a href='/posts/{{ $post->id }}'>{{ $post->title }}</a>
+                    <a href='/posts/{{ $post->id }}'>{{ $post->title }}</a>
                     <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">削除</button> 
+                    </form>
                 </div>
             @endforeach
         </div>
@@ -23,5 +27,14 @@
         <div class='create'>
             <a href='/posts/create'>create</a>
         </div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+        
+                if (confirm('削除すると復元できません。\n本当に削除しますか？')) {
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
